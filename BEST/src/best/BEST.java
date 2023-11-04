8a7726e66ba403e0b0e827d7741d361c7fa885bc
leozamboni <leonardonunes169@gmail.com>
2023-11-04 15:56:29 -0300

    FEAT: continuando

--- BEST/src/best/BEST.java
+++ BEST/src/best/BEST.java
@@ -4,6 +4,7 @@
  */
 package best;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,17 +17,21 @@ public class BEST {
 
     static class AINP {
 
-        static int SIGFIT, IS, IC, VB, PH, LBETA, MMC, NAME, TITLE;
+        static int SIGFIT, IC, PH, MMC, NAME, TITLE;
+        static int IS[][];
+        static double[] LBETA;
+        static int[] VB;
     }
 
     static class AAAA {
 
-        static int BELL, I1, I2, ISTEP, NDP, NS, NC, AA, RCC, T;
+        static int BELL, I1, I2, ISTEP, NDP, NS, NC, AA, T;
+        static double[][] RCC;
     }
 
     static class BBBB {
 
-        static int PHCOR, VO, NB, MMACID;
+        static int PHCOR, NB, VO, MMACID;
     }
 
     static class CCCC {
@@ -55,7 +60,40 @@ public class BEST {
         static int LUNI, LUNO;
     }
 
+    static class HHHH {
+
+        static int DLB, KCVERR, BETERR, SGF, BERBET;
+    }
+
+    int I, K;
     int K1, K2, KSTEP = 0;
+    int MM;
+
+// TODO: INPUT ROUTINE (Página 18 do livro)
+//    int V, CB;
+//    void INPUT() { // 111:
+//        I = 1;
+//        for (; I <= AAAA.NDP; I++) {
+//            V = BBBB.VO + AINP.VB[I];
+//            CB = AINP.VB[I] * BBBB.NB / V;
+//            K = 1;
+//            for (; K <= 1; K++) {
+//                if (MM == 0 && AAAA.RCC[I][K] < 0) {
+//                    AAAA.RCC[I][K] = Math.pow(10, AAAA.RCC[I][K]);
+//                }
+//            }
+//        }
+//    }
+    
+    void BODY() { // SUBROUNTINE BODY (Página 22 do livro)
+        int NFIR = 1;
+        double SW = 0;
+        
+        if (N == -2 || N == 1) { 
+            NFIR = 1; 
+        }
+            
+    }
 
     public BEST() {
         GGGG.NREF = 0;
@@ -64,18 +102,45 @@ public class BEST {
         LLLL.LUNO = 6;
         DDDD.FAST = 1;
 
-        this.OPTIONS();
+        System.out.println("PROGRAMA BEST");
+        System.out.println("RAMUNMAS J. MONTEKAITIS");
+        System.out.println("VERSION 10/10/91");
+        System.out.println("REAL*8 VARIABLES");
+
+        System.out.println("FILE USAGE:");
+        System.out.println("FOR001.DAT - OUTPUT");
+        System.out.println("FOR002.DAT - SCRATCH (COMPONENTS)");
+        System.out.println("FOR003.DAT - SCRATCH (LOG BETA VALUES)");
+        System.out.println("FOR004.DAT - INPUT (ORIGINAL DATA)");
+        System.out.println("ERROR.DAT - (LOGGING OPTION 31)");
+
+        K = 1;
+        for (; K <= 7; K++) {
+            I = 1;
+            for (; I <= 111; I++) {
+                AAAA.RCC[I][K] = 0;
+            }
+        }
+
+        MM = 0;
+
+        // CALL INPUT
+        AAAA.I1 = 1;
+        AAAA.I2 = AAAA.NDP;
+        AAAA.ISTEP = 1;
+
+        this.OPTION(); // CALL OPTION
     }
 
     int MCH;
+    int NDUM;
 
-    /**
-     * @param args the command line arguments
-     */
-    void OPTIONS() { // 70:
+    void OPTION() { // SUBROUNTINE OPTION 70: 
         System.out.println("OPTIONS: ");
         MCH = Integer.parseInt(scan.nextLine());
+        NDUM = 1;
 
+        // CALL BODY
         switch (MCH) {
             case 14: // GO TO 5300
                 break;
@@ -119,7 +184,7 @@ public class BEST {
             case 31: // "ERROR ANALYSIS" CALL ERROR
                 break;
         }
-        this.OPTIONS(); // GO TO 70
+        this.OPTION(); // GO TO 70
         // CONTINUE
     }
 
@@ -138,11 +203,11 @@ public class BEST {
             DDDD.FAST = Math.pow(10, I);
         }
         ISPEED = I;
-        this.OPTIONS(); // GO TO 70
+        this.OPTION(); // GO TO 70
     }
 
     void POINTS() { // 18:
-        System.out.println("INITIAL, FINAL (or -1), STEP (312) or RETURN"); // 10:
+        System.out.println("INITIAL, FINAL (or -1), STEP (3I2) or RETURN"); // 10:
         String[] inp = scan.nextLine().split(" ", 3);
         K1 = Integer.parseInt(inp[0]);
         K2 = Integer.parseInt(inp[1]);
@@ -154,26 +219,34 @@ public class BEST {
             AAAA.I2 = K2;
         }
         if (KSTEP > 0) {
-            ISPEED = KSTEP;
+            AAAA.ISTEP = KSTEP;
         }
         if (K2 < 0) {
             AAAA.I2 = AAAA.NDP;
         }
-        this.OPTIONS(); // GO TO 70
+        this.OPTION(); // GO TO 70
     }
-    
+
     void BETA_VALUES() { // 59:
+        // TODO
+//        for (int i = 1; i < AAAA.NS; i++) {
+//            System.out.println(AINP.LBETA[i]);
+//            System.out.println(IS[I, K
+//            ]);
+//            
+//            System.out.println(, NAME(K), K = 1, NC);
+//        }
         while (true) { // 48:
             System.out.println("ENTER \"I\" OF LOG BETA TO CHANGED or RETURN"); // 49:
             int I = Integer.parseInt(scan.nextLine());
-            if (I < 1) { 
-                this.OPTIONS(); // GO TO 70
+            if (I < 1) {
+                this.OPTION(); // GO TO 70
             }
-            if (I > AAAA.NS) { 
+            if (I > AAAA.NS) {
                 continue; // GO TO 48
             }
-            System.out.println("ENTER NEW LOG BETA"); // 53:
-            AINP.LBETA = Integer.parseInt(scan.nextLine());
+            System.out.println("ENTER NEW LOG BETA (" + AAAA.I2 + ")?"); // 53:
+            AINP.LBETA[I] = Integer.parseInt(scan.nextLine());
             // GO TO 59
         }
     }
